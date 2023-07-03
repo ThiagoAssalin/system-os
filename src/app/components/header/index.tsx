578b733ce5b1615bc356e401eb793c804d20072c
ThiagoAssalin <thiagos.assalin@gmail.com.com>
2023-07-03 17:19:59 -0300

    FEAT - criando Header

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+
+export default function Header(){
+    return(
+        <header className=" w-screen h-16 flex items-center justify-between p-2 border-b-2">
+            <h1>OsSystem</h1>
+            <nav className="flex gap-2">
+                <Link href={''}>DashBoard</Link>
+                <Link href={''}>Login</Link>
+            </nav>
+        </header>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from './components/header'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = {
   title: 'SystemOS',
@@ -13,9 +13,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
+  return (    
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Header/>
+        {children}
+      </body>
     </html>
   )
 }
