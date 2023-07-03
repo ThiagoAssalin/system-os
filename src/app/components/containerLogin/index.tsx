0920c565d290e4aee1376629e6e421e75e61e3bd
ThiagoAssalin <thiagos.assalin@gmail.com.com>
2023-07-03 17:19:14 -0300

    FEAT- criando header

---
+++ src/app/components/containerLogin/index.tsx
@@ -0,0 +1,11 @@
+export default function LoginContainer(){
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="Login"></input>
+                <input type="text" placeholder="Password"></input>
+                <input type="submit"></input>
+            </form>      
+        </div>
+    )
+}
---
+++ src/app/pages/login.tsx
@@ -0,0 +1,9 @@
+import LoginContainer from "@/app/components/containerLogin";
+
+export default function Login(){
+    return(
+        <div>
+            <LoginContainer/>
+        </div>
+    )
+}
