ee3c45c309c4015342aef8ca20269200d46c37c2
ThiagoAssalin <thiagos.assalin@gmail.com.com>
2023-08-17 18:15:27 -0300

    REFACTOR- Ajustando renderisação condicional e tipando props

--- src/app/components/cardOs/index.tsx
+++ src/app/components/cardOs/index.tsx
@@ -1,13 +1,21 @@
 import { AiFillDelete } from "react-icons/ai";
 
-export default function CardOs({id, requester, finished}){
+
+export default function CardOs({id, requester, finished}:{id:number, requester:string, finished:boolean}){
 
     var complete
 
     if(finished === true){
-        return complete = <p className=" text-xs">completo</p>
+        return complete = 
+        <div className="flex justify-center items-center gap-1">
+            <div className=" bg-green-500 w-2 h-2 rounded"></div>
+            <p className=" text-xs">completo</p>
+        </div>
     }else{
-        complete = <p className=" text-xs">incompleta</p>
+        complete = <div className="flex justify-center items-center gap-1">
+                        <div className=" bg-yellow-500 w-2 h-2 rounded"></div>
+                        <p className=" text-xs">incompleto</p>
+                    </div>
     }
 
 
@@ -19,10 +27,7 @@ export default function CardOs({id, requester, finished}){
                 <p className=" text-xs">Nome: {requester}</p>
             </div>
             <div className="flex flex-col justify-center items-end gap-2 ">
-                <div className="flex justify-center items-center gap-1">
-                    <div className=" bg-green-500 w-2 h-2 rounded"></div>
                     {complete}
-                </div>
                     <button><AiFillDelete/></button>
             </div>
         </div>
