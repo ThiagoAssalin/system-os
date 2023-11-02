0a6dde05e0ba8e14f147f69e7e79364b20556bf7
ThiagoAssalin <thiagos.assalin@gmail.com.com>
2023-11-02 19:06:40 -0300

    Feat - Criando Profile card

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,7 +1,36 @@
+'use client'
+
 import Link from "next/link";
+import { useAuth } from "@/services/authContext"
+import { useState } from "react";
+import ProfileCard from "../profileCard";
 
 
 export default function Header(){
+    const [isOpen, setIsOpen] = useState(false)
+    const {isAuthenticated} = useAuth()
+
+    function handleClick(){
+        setIsOpen(!isOpen)
+    }
+
+    if(isAuthenticated){
+        return(
+            <header className="  h-16 flex items-center justify-between p-4 border-b-2 fixed w-screen top-0 ">
+                <h1>OsSystem</h1>
+                <nav className="flex gap-2">
+                    <button onClick={handleClick} className="flex flex-col justify-center items-center">
+                        Admin   
+                    </button>
+                    {isOpen?<div className="absolute right-1 top-16">
+                        <ProfileCard/>
+                    </div>:<></>}
+                 </nav>
+                
+                    
+            </header>
+        )
+    }
     return(
         <header className="  h-16 flex items-center justify-between p-4 border-b-2 fixed w-screen top-0 ">
             <h1>OsSystem</h1>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -20,13 +20,13 @@ export default function Dashboard() {
 
   return (
     <PrivateRoutes>
-    <main className="flex justify-center items-start absolute top-28 w-screen">
-      <div className="fixed bottom-0 right-0 m-3">
-        <button className="border rounded-md text-white bg-cyan-500 p-1" onClick={()=>setVisible(true)}>Criar OS</button>
-      </div>
-      <Modal visible={visible} closeHandler={closeHandler}></Modal>
-      <OsList/>
-    </main>
+      <main className="flex justify-center items-start absolute top-28 w-screen">
+        <div className="fixed bottom-0 right-0 m-3">
+          <button className="border rounded-md text-white bg-cyan-500 p-1" onClick={()=>setVisible(true)}>Criar OS</button>
+        </div>
+        <Modal visible={visible} closeHandler={closeHandler}></Modal>
+        <OsList/>
+      </main>
     </PrivateRoutes>
   )
 }
